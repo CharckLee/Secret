981d32948eb218637b95c323fbc3ac97292e118a
Charck <1354408237@qq.com>
2026-08-21 02:12:55 +0800

    chore: untrack search_essay leftovers

--- search_essay/prisma/migrations/20260803014416_add_translated_content/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Literature" ADD COLUMN "translatedContent" TEXT;
--- search_essay/src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingSpinner } from '@/components/ui/LoadingSpinner';
-
-export default function GlobalLoading() {
-  return (
-    <div className="min-h-screen bg-slate-50 flex items-center justify-center">
-      <LoadingSpinner size="lg" text="LitMind 加载中..." />
-    </div>
-  );
-}
